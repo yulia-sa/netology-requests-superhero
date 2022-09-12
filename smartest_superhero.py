96e9ffe797ac59931f1fb962f69723c23b73694e
Yulia S <sav.jul@gmail.com>
2022-09-13 01:03:12 +0300

    Handle case with several smartest superheroes

--- smartest_superhero.py
+++ smartest_superhero.py
@@ -12,7 +12,7 @@ def get_all_superheroes_info(url):
         return None
 
     all_superheroes_info = all_superheroes_response.json()
-    
+
     return all_superheroes_info
 
 
@@ -22,9 +22,14 @@ def find_smartest_superhero(superheroes_list, all_superheroes_info):
 
     for superhero in all_superheroes_info:
         if superhero["name"] in superheroes_list:
+            superhero_intelligence = superhero["powerstats"]["intelligence"]
             if not smartest_superhero_intelligence or \
-                    superhero["powerstats"]["intelligence"] > smartest_superhero_intelligence:
+                    superhero_intelligence > smartest_superhero_intelligence:
                 smartest_superhero = superhero["name"]
+                smartest_superhero_intelligence = superhero_intelligence
+                continue
+            if superhero_intelligence == smartest_superhero_intelligence:
+                smartest_superhero += ", " + superhero["name"]
 
     return smartest_superhero
 
@@ -32,7 +37,7 @@ def find_smartest_superhero(superheroes_list, all_superheroes_info):
 def main():
     all_superheroes_info = get_all_superheroes_info(ALL_SUPERHEROES_URL)
     smartest_superhero = find_smartest_superhero(SUPERHEROES_FOR_COMPARE, all_superheroes_info)
-    print(f"Самый умный супергерой — {smartest_superhero}")
+    print(f"Самый умный супергерой (супергерои) — {smartest_superhero}")
 
 
 if __name__ == "__main__":
